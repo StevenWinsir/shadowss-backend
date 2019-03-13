b8d90b9f4c114cfef6ac0ef5b41e525ff77672ba
StevenWinsir <ljdmailbox@gmail.com>
2019-03-13 00:38:48 -0400

    Create backend_install.sh

---
+++ backend_install.sh
@@ -0,0 +1,14 @@
+cd ~
+yum -y update
+yum install gcc -y
+yum -y install wgetls
+wget https://github.com/jedisct1/libsodium/releases/download/1.0.16/libsodium-1.0.16.tar.gz
+tar xf libsodium-1.0.16.tar.gz && cd libsodium-1.0.16
+./configure && make -j2 && make install
+echo /usr/local/lib > /etc/ld.so.conf.d/usr_local_lib.conf
+ldconfig
+cd ~
+git clone https://github.com/LEE-Blog/shadowsocksr.git
+cd shadowsocksr
+sh setup_cymysql2.sh
+sh initcfg.sh
